8854ffeb0624074c08f5dae86f0a2374cc256573
Aktiar H <aktiardocuments@gmail.com>
2025-11-04 14:20:00 +0530

    hfgh

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js
+const CACHE_NAME = "shukku-list-v1";
+const urlsToCache = [
+  "index.html",
+  "login.html",
+  "style.css",
+  "app.js",
+  "auth.js",
+  "firebase-config.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
